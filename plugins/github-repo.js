3b8f82c3536d7132697c3e0261cc26ae83b55a03
Evgene Leontiev <potehin@gmail.com>
2020-11-15 23:19:45 +0400

    add github api + component (#6)
    * add github api + component
    
    * make linters green again

---
+++ plugins/github-repo.js
@@ -0,0 +1,16 @@
+import GithubApi from '~/plugins/github-api'
+
+export default {
+  getOrgInfo() {
+    return GithubApi().get('/orgs/dev-pet-projects')
+  },
+  getOrgMembers() {
+    return GithubApi().get('/orgs/dev-pet-projects/public_members')
+  },
+  getRepositories() {
+    return GithubApi().get('/orgs/dev-pet-projects/repos')
+  },
+  getContributors(repoName) {
+    return GithubApi().get('/repos/' + repoName + '/contributors')
+  },
+}
